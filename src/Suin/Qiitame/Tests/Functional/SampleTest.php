29f7d847ebb4a4b3032ee28f738dd1245dd0522e
suin <suinyeze@gmail.com>
2013-03-30 01:42:00 +0900

    set up project

---
+++ src/Suin/Qiitame/Tests/Functional/SampleTest.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Suin\Qiitame\Tests\Functional;
+
+class SampleTest extends \PHPUnit_Framework_TestCase
+{
+    public function testSample()
+    {
+        $this->assertTrue(false);
+    }
+}
